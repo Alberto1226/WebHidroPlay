fa90e028b20ab90b9afaaeb3f3c6adf69afb123f
Alberto1226 <a15021347@iteshu.edu.mx>
2025-02-05 11:01:20 -0600

    cambios en config of vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3300, // Ahora Vite siempre usará el puerto 3300
+    host: true, // Permite acceso desde la red
+  }
 })
